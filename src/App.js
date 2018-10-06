fa3cabf21bae1414bcd593298983b9d756835514
Drew Stonebraker <dstonebraker@restorationhardware.com>
2018-10-06 14:01:32 -0700

    Add packages

--- src/App.js
+++ src/App.js
@@ -1,27 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 
 class App extends Component {
   render() {
-    const hello = 'hi';
-    const hello = 'foo';
-    console.log(
-      'aspdoifj  pasodfij paosdifj ',
-      ' poaisdj fpoiajsdf poiaj asdfpioj ',
-      'poaijsd fpoijfads ',
-    );
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          {' '}
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <div />;
   }
 }
 
